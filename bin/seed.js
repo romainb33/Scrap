36eac21a57ea25617646fd4b1ca0a510989d59f8
ronronscelestes <julie.plantey@gmail.com>
2021-03-06 17:56:41 +0100

    refactored seed

--- bin/seed.js
+++ bin/seed.js
@@ -3,92 +3,45 @@ const axios = require("axios");
 
 const GameModel = require("./../models/Game.model");
 
-let gamesData = [];
+let gameData;
 
-const scrapGames = (page) => {
-  axios.get(`https://api.rawg.io/api/games?page=${page}&key=95fc1125400b41b6ac588703a63dd1fe`)
-  .then(apiRes => {
-    apiRes.data.results.forEach(result => {
-    axios.get(`https://api.rawg.io/api/games/${result.id}?key=95fc1125400b41b6ac588703a63dd1fe`)
-    .then(resultId => {
-      // //extracting year
-      let yearArr;
-      let yearReleased;
-      if (resultId.data.released) {
-        yearArr = resultId.data.released.split('-');
-        yearReleased = Number(yearArr[0]);
-      }
-      // //extracting developers
-      const developersArr = [];
-      resultId.data.developers.forEach(developer => developersArr.push(developer.name));
-      //extracting genres
-      const genresArr = [];
-      resultId.data.genres.forEach(genre => genresArr.push(genre.name));
-      //extracting platforms
-      const platformsArr = [];
-      resultId.data.platforms.forEach(result => platformsArr.push(result.platform.name));
-      let gameData = {
-        name : resultId.data.name,
-        slug : resultId.data.slug,
-        description : resultId.data.description_raw,
-        background_image : resultId.data.background_image,
-        developers : developersArr,
-        metacritic : resultId.data.metacritic,
-        genres : genresArr,
-        platforms : platformsArr,
-        website : resultId.data.website,
-        released : yearReleased
-      }
-      gamesData.push(gameData);
-    })
+const scrapGames = async (page) => {
+
+  try {
+    const apiRes = await axios.get(`https://api.rawg.io/api/games?page=${page}&key=6aa995502d114491a15ad90bb264dc2f`);
+
+    apiRes.data.results.forEach(async result => {
+      const game = await axios.get(`https://api.rawg.io/api/games/${result.id}?key=6aa995502d114491a15ad90bb264dc2f`)
+
+      const { name, slug, description_raw, background_image, metacritic, website, released, developers, genres, platforms } = game.data;
+
+      gameData = {
+        name : name,
+        slug : slug,
+        description : description_raw,
+        background_image : background_image,
+        metacritic : metacritic,
+        website : website,
+        released: Number(released.split('-')[0]),
+        developers: developers.map( ({name}) => name ),
+        genres: genres.map( ({name}) => name ),
+        platforms: platforms.map( item => item.platform.name ),
+      };
+
+      // await GameModel.insertOne(gameData);
+
+      GameModel.insertOne(gameData)
+      .then(dbSucces => console.log('inserted 1'))
+      .catch(err => console.log('cannot insert this one'));
+
     });
-  })
-  .catch(err => console.log(err));
-}
 
-// const createGameData = () => {
-//   for(let i = 1; i < 6; i++) {
-//     scrapGames(i);
-//   }
-//   return gamesData
-// }
-// const allGamesArr = createGameData();
+  } catch(err) {
+    console.log(err);
+  }
+  
+}
 
 for(let i = 1; i <= 400; i++) {
   scrapGames(i);
 }
-
-setTimeout(() => {
-  GameModel.insertMany(gamesData)
-    .then(dbSucess => console.log("Done"))
-    .catch(err => console.log(err))
-}, 120000)
-
-// const createGameData = () => {
-//   console.log("createGameData called")
-//   for(let i = 1; i < 6; i++) {
-//     scrapGames(i);
-//   }
-//   return gamesData
-// }
-// let datas;
-
-// const insertData = async () => {
-//   console.log("insertData called")
-//   datas = await createGameData();
-//   GameModel.insertMany(datas)
-//     .then(dbSucess => console.log(dbSucess))
-//     .catch(err => console.log(err))
-// }
-// insertData()
-//
-// (async function insertLabels() {
-//   try {
-//     await GameModel.deleteMany(); // empty the album db collection
-
-//     const inserted = await GameModel.insertMany(gamesData); // insert docs in db
-//     console.log(`seed albums done : ${inserted.length} documents inserted !`);
-//   } catch (err) {
-//     console.error(err);
-//   }
-// })();
